f3a4b618bc60d5ae475b643a48b794abc56af9d5
jesse collier <jesse.collier97@gmail.com>
2019-04-19 11:38:29 +1200

    stash

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,15 +9,41 @@ import LoadingBar from "./LoadingBar";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+
+      api: {
+        // imageLink:
+        //   "https://i.kinja-img.com/gawker-media/image/upload/s--CWKHA2W9--/c_scale,f_auto,fl_progressive,q_80,w_800/yxeok4wpwe54cn9y05j1.jpg",
+        // title: "bird",
+        // description: "this bird has a beak"
+      },
+      loading: false
+    };
   }
+  updateImageState(imageLink, title, description) {
+    this.setState({
+      api: {
+        imageLink,
+        title,
+        description
+      },
+      loading:false
+    });
+  }
+
+  showLoadingBar() {
+    this.setState({ loading: true });
+  }
+
+
   render() {
+    console.log(this.state);
     return (
       <React.Fragment>
         <Header />
-        <ImageButton />
-        <ImagePage />
-        <LoadingBar />
+        <ImageButton onButton={this.updateImageState.bind(this)} />
+        { this.state.api.imageLink && <ImagePage imageData={this.state.api} />}
+        {this.state.loading && <LoadingBar />}
         <Footer />
       </React.Fragment>
     );
--- client/components/ImageButton.jsx
+++ client/components/ImageButton.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import { directive } from "@babel/types";
 
+const fakeData = {
+  imageLink:
+    "https://i.kinja-img.com/gawker-media/image/upload/s--CWKHA2W9--/c_scale,f_auto,fl_progressive,q_80,w_800/yxeok4wpwe54cn9y05j1.jpg",
+  title: "bird",
+  description: "this bird has a beak"
+};
+
 class ImageButton extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  handleClick() {
+    let { imageLink, title, description } = fakeData;
+    this.props.onButton(imageLink, title, description);
+  }
+
+
   render() {
-    return <p>image button is here</p>;
+    return <button onClick={this.handleClick.bind(this)}>Get Image</button>;
   }
 }
 
