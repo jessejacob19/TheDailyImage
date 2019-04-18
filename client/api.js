62e1ccd9ba1c2a0264602493b6349a4f051a7f22
Josh Gulliver <Joshgull23@Joshs-Air-2.localdomain>
2019-04-19 10:17:20 +1200

    api.js added

---
+++ client/api.js
@@ -0,0 +1,11 @@
+import request from "superagent";
+
+const randomPictures = "https://api.unsplash.com/photos/?client_id=d9542b388f7922420a51b5739d57f04affc18be56c1b39ffb48063aafa003f17";
+
+export function getInfo(callback) {
+  request.get(randomPictures).end((err, res) => {
+    callback(err, res.body);
+  });
+}
+
+export default getInfo;
